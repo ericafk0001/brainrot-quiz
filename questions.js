df206f5baa046fcc2e2bb480ad1895e3b99d385e
ericafk0001 <dxslyofficial@gmail.com>
2024-11-20 20:50:48 -0500

    Add randomizer and sound fx

---
+++ questions.js
@@ -0,0 +1,167 @@
+const questions = [
+  {
+    question: 'Which Internet Celebrity said "Hawk TUAH Spit on that Thang!"?',
+    answers: [
+      { text: "Whitney Cummings", correct: false },
+      { text: "Haliey Welch", correct: true },
+      { text: "Jake Paul", correct: false },
+      { text: "Sydney Sweeney", correct: false },
+    ],
+  },
+  {
+    question: 'What happens when someone says "I Understand It Now"?',
+    answers: [
+      { text: "They become an NBA athlete", correct: false },
+      { text: "They accept their death", correct: false },
+      { text: "They get extremely good at what their doing", correct: true },
+      { text: "They achieve inner peace", correct: false },
+    ],
+  },
+  {
+    question:
+      "Fill in the blank: PUT THE MANGO DOWN! THIS IS FOR MY _____ ___!",
+    answers: [
+      { text: "WINTER ARC", correct: true },
+      { text: "SKIBIDI RIZZ", correct: false },
+      { text: "MOM SIR", correct: false },
+      { text: "FORTNITE KITTEN", correct: false },
+    ],
+  },
+  {
+    question: "Fill in the blank: That feeling when ____ ______ is tommorow",
+    answers: [
+      { text: "knee sugery", correct: true },
+      { text: "your marriage", correct: false },
+      { text: "gritty performance", correct: false },
+      { text: "feet licking", correct: false },
+    ],
+  },
+  {
+    question: "Fill in the blank: No ___ __________",
+    answers: [
+      { text: "New Negativity", correct: false },
+      { text: "Net Lease", correct: false },
+      { text: "Nut November", correct: true },
+      { text: "Naughty Night", correct: false },
+    ],
+  },
+  {
+    question: 'Who is the purple guy that gets played with the song "Whistle"?',
+    answers: [
+      { text: "John Smith", correct: false },
+      { text: "John Doe", correct: false },
+      { text: "John Hutcherson", correct: true },
+      { text: "John Pork", correct: false },
+    ],
+  },
+  {
+    question: "What was the song playing at the Tiktok Rizz Party?",
+    answers: [
+      { text: "Took Her To The O by King Von", correct: false },
+      { text: "If We Being Rëal by Yeat", correct: false },
+      { text: "FE!N by Travis Scott", correct: false },
+      { text: "Carnival By Kanye", correct: true },
+    ],
+  },
+  {
+    question: "Who is the boy with the red red red face?",
+    answers: [
+      { text: "Tomato Boy", correct: true },
+      { text: "Firetruck Optimus Prime", correct: false },
+      { text: "Senpai", correct: false },
+      { text: "Playboy Carti", correct: false },
+    ],
+  },
+  {
+    question: "Who is the leader of the tiktok rizz party?",
+    answers: [
+      { text: "Red Tie Kid", correct: false },
+      { text: "Blue Tie Kid", correct: true },
+      { text: "Togger the Mogger", correct: false },
+      { text: "Bluddy", correct: false },
+    ],
+  },
+  {
+    question:
+      "What ethnicity was the quandale dingle at the tiktok rizz party?",
+    answers: [
+      { text: "Turkish", correct: true },
+      { text: "Chinese", correct: false },
+      { text: "Kazakhstanian", correct: false },
+      { text: "Nigerian", correct: false },
+    ],
+  },
+  {
+    question: 'Who sang this song? "skibidi dop-dop-dop yes yes.."',
+    answers: [
+      { text: "Kai Cenat", correct: false },
+      { text: "Skibidi Rizzler", correct: false },
+      { text: "Skibidi Hydra", correct: false },
+      { text: "Skibidi Toilet", correct: true },
+    ],
+  },
+  {
+    question: "How many skibidi toilets are there?",
+    answers: [
+      { text: "8", correct: false },
+      { text: "420", correct: false },
+      { text: "69", correct: false },
+      { text: "77", correct: true },
+    ],
+  },
+  {
+    question: "What is the multi-head skibidi toilet called?",
+    answers: [
+      { text: "YN Skibidi", correct: false },
+      { text: "Skibidiest of the Skibidi's", correct: false },
+      { text: "The hydra skibidi toilet.", correct: true },
+      { text: "Skibidi toilet king", correct: false },
+    ],
+  },
+  {
+    question: "Who is the king of the chicken bake?",
+    answers: [
+      { text: "The Costco Guys", correct: true },
+      { text: "Young Neighbor", correct: false },
+      { text: "Duke Dennis", correct: false },
+      { text: "Hawk Tuah Girl", correct: false },
+    ],
+  },
+  {
+    question: 'Fill in the blank: "Dang, Look at his ____!!"',
+    answers: [
+      { text: "Swag", correct: false },
+      { text: "Drip", correct: false },
+      { text: "Rizz", correct: true },
+      { text: "Huzz", correct: false },
+    ],
+  },
+  {
+    question: 'Fill in the blank: "Sticking out your ____ for the ____"',
+    answers: [
+      { text: "Gyat, Rizzler", correct: true },
+      { text: "Stick, Huzz", correct: false },
+      { text: "Money, Grandma", correct: false },
+      { text: "Knee, Sugery", correct: false },
+    ],
+  },
+  {
+    question: 'Fill in the blank: "hey... its _______ calling..."',
+    answers: [
+      { text: "Ray", correct: false },
+      { text: "Freakbob", correct: true },
+      { text: "Drake", correct: false },
+      { text: "Your Mom", correct: false },
+    ],
+  },
+  {
+    question:
+      'Fill in the blank: "She wanna have my kids, OMG, ____ ___ ___ ____"',
+    answers: [
+      { text: "Talk Tuah Momma", correct: false },
+      { text: "Sean John Combs", correct: false },
+      { text: "Jace got W rizz", correct: true },
+      { text: "Grace got wizz", correct: false },
+    ],
+  },
+];
