0b1ca1fb5828249191f0eb3a144dc8b2b3838951
ericafk0001 <dxslyofficial@gmail.com>
2024-11-20 21:28:55 -0500

    Add Questions + Small Edit

--- questions.js
+++ questions.js
@@ -164,4 +164,169 @@ const questions = [
       { text: "Grace got wizz", correct: false },
     ],
   },
+  {
+    question: "What song plays when Lebron is being absorbed by light?",
+    answers: [
+      { text: "You are my song", correct: false },
+      { text: "You are my sunshine", correct: true },
+      { text: "You are my everything", correct: false },
+      { text: "You are my goal", correct: false },
+    ],
+  },
+  {
+    question: "What does the orange nugget with a roblox man face say?",
+    answers: [
+      { text: "Kneel Down BAKA!", correct: false },
+      { text: "Oi Oi Oi", correct: false },
+      { text: "Gedagedigedagedago", correct: true },
+      { text: "JaJaJajaJa", correct: false },
+    ],
+  },
+  {
+    question: "What does the red larva say?",
+    answers: [
+      { text: "That's sus", correct: false },
+      { text: "Oi Oi Oi", correct: true },
+      { text: "Hawk TUAH Spit on that thang!", correct: false },
+      { text: "Yellow Larva your done.", correct: false },
+    ],
+  },
+  {
+    question:
+      'Fill in the blank: "When she ruins your life but your lowkey just __ ____ ____"',
+    answers: [
+      { text: "lowkey a chill dude", correct: true },
+      { text: "a nonchalant dreadhead", correct: false },
+      { text: "a sigma who dont care", correct: false },
+      { text: "a freshmen", correct: false },
+    ],
+  },
+  {
+    question: "What is the Broccoli in the Hot Tub called?",
+    answers: [
+      { text: "Black Broccoli", correct: false },
+      { text: "Dripped out Broccoli", correct: false },
+      { text: "Water Broccoli", correct: false },
+      { text: "Gangsta Broccoli", correct: true },
+    ],
+  },
+  {
+    question: "What is the green caterpillar with a cig called?",
+    answers: [
+      { text: "Kratos", correct: false },
+      { text: "Kennick", correct: false },
+      { text: "Krater", correct: true },
+      { text: "Karson", correct: false },
+    ],
+  },
+  {
+    question: "I just lost __ ____",
+    answers: [
+      { text: "my bust", correct: false },
+      { text: "my huzz", correct: false },
+      { text: "my dawg", correct: true },
+      { text: "my wife", correct: false },
+    ],
+  },
+  {
+    question: "Who rizzed up level 10 gyat Livvy Dunne",
+    answers: [
+      { text: "Baby Groot", correct: false },
+      { text: "Lil baby", correct: false },
+      { text: "Baby Gronk", correct: true },
+      { text: "Dababy", correct: false },
+    ],
+  },
+  {
+    question: 'Fill in the blank: "Erm, what the _____"',
+    answers: [
+      { text: "skibidi", correct: false },
+      { text: "sigma", correct: true },
+      { text: "heck", correct: false },
+      { text: "flip", correct: false },
+    ],
+  },
+  {
+    question: "What is it called when you beat your meat very often?",
+    answers: [
+      { text: "diddling", correct: false },
+      { text: "gooning", correct: true },
+      { text: "feining", correct: false },
+      { text: "tripping", correct: false },
+    ],
+  },
+  {
+    question: "What youtuber is two steps ahead?",
+    answers: [
+      { text: "Diddy", correct: false },
+      { text: "Nikocado Avacado", correct: true },
+      { text: "Sam Sulek", correct: false },
+      { text: "Robert Fanum Escanio", correct: false },
+    ],
+  },
+  {
+    question: "Who's mass is so huge it creates a blackhole?",
+    answers: [
+      { text: "Nikocado Avacado", correct: false },
+      { text: "TenZ", correct: false },
+      { text: "Sam Sulek", correct: false },
+      { text: "Caseoh", correct: true },
+    ],
+  },
+  {
+    question: "What is it called when someone takes your food?",
+    answers: [
+      { text: "YN Tax", correct: false },
+      { text: "Youngboul Tax", correct: false },
+      { text: "Food Tax", correct: false },
+      { text: "Fanum tax", correct: true },
+    ],
+  },
+  {
+    question: "What is it called when you stretch your Johnson?",
+    answers: [
+      { text: "Jelqing", correct: true },
+      { text: "Diddling", correct: false },
+      { text: "Joking", correct: false },
+      { text: "Gooning", correct: false },
+    ],
+  },
+  {
+    question:
+      "What is it called when you look way better than someone next to you?",
+    answers: [
+      { text: "Mogging", correct: true },
+      { text: "Diddling", correct: false },
+      { text: "Looksmaxxing", correct: false },
+      { text: "Gooning", correct: false },
+    ],
+  },
+  {
+    question: "Who does everyone want to work with?",
+    answers: [
+      { text: "Natalie Tran", correct: false },
+      { text: "Sigma", correct: false },
+      { text: "Nonchalant Dreadhead", correct: true },
+      { text: "Alpha Wolf", correct: false },
+    ],
+  },
+  {
+    question:
+      'What song is this? "From the screen to the ring, to the pen, to the king"',
+    answers: [
+      { text: "Carnival By Kanye", correct: false },
+      { text: "FE!N By Travis Scott", correct: false },
+      { text: "Thick Of It By KSI", correct: true },
+      { text: "New By Ken Carson", correct: false },
+    ],
+  },
+  {
+    question: "Who escaped the matrix?",
+    answers: [
+      { text: "Andrew Tate", correct: true },
+      { text: "Kai Cenat", correct: false },
+      { text: "NBA Youngboy", correct: false },
+      { text: "Mark Zuckerberg", correct: false },
+    ],
+  },
 ];
